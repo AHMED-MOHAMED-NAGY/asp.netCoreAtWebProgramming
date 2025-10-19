e39c573f2421c1474c8bd9713eb2969dbb3c0a63
ahmed <ahmedmohamed6x5@gmail.com>
2025-10-19 08:33:43 +0300

    create new MVC application and create a controller

---
+++ H2B1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace H2B1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ H2B1/Models/Ogrenci.cs
@@ -0,0 +1,7 @@
+﻿namespace H2B1.Models
+{
+    public class Ogrenci
+    {
+        public String ogrName { get; set; }
+    }
+}
