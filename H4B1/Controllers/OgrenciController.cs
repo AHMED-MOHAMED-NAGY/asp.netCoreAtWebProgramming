7da9ce6b80371673511593c9bdcd56bc478daf38
ahmed <ahmedmohamed6x5@gmail.com>
2025-10-21 12:52:20 +0300

    the code that writen in 4.week

---
+++ H4B1/Controllers/OgrenciController.cs
@@ -0,0 +1,79 @@
+﻿using H4B1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace H4B1.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public static List<Ogrenci> ogrenciler = new List<Ogrenci>(); // reset yada refreach yapildiginda hersey gider bir request 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ogrEkle()
+        {
+            return View();
+        }
+
+        //[HttpGet] varsa mantikli degil cunku defualt olarak get ile calisir
+
+        // for get
+        //public string ogrKaydet() {
+        //    string ad = HttpContext.Request.Query["ogrAd"];
+        //    string soyad = HttpContext.Request.Query["ogrSoyad"];
+        //    string no = HttpContext.Request.Query["ogrNo"];
+        //    return ad+ " " + soyad + " "+ no;
+        //}
+
+        [HttpPost] // http post varsa calissin
+        public string ogrKaydet() {
+            string ad = HttpContext.Request.Form["ogrAd"];
+            string soyad = HttpContext.Request.Form["ogrSoyad"];
+            string no = HttpContext.Request.Form["ogrNo"];
+
+            // data temizleme 
+            // DB kayet
+            return ad + " " + soyad + " " + no;
+        }
+
+        public string OgrKaydetParams(string ogrAdi, string ogrSoyad, string ogrNo){
+            string ad = ogrAdi;
+            string soyad = ogrSoyad;
+            string no = ogrNo;
+            return ad + " " + soyad +" "+ no;
+        }
+
+        public string OgrKaydetClass(Ogrenci ogr) 
+        {
+            string ad = ogr.ogrAdi;
+            string soyad = ogr.ogrSoyad;
+            string no = ogr.ogrNo;
+            return ad + " " + soyad +" "+ no;
+        }
+
+        public IActionResult OgrKaydetClassView(Ogrenci ogr) 
+        {
+            string ad = ogr.ogrAdi;
+            string soyad = ogr.ogrSoyad;
+            string no = ogr.ogrNo;
+            if (string.IsNullOrEmpty(ad)) {
+                TempData["Hata"] = "name is null or empty";
+                return RedirectToAction("ogrHata");
+            }
+            return View(ogr);
+        }
+        public IActionResult OgrKaydetClassViewAll(Ogrenci ogr) {
+            ogrenciler.Add(ogr);
+            TempData["msj"] = ogr.ogrAdi + " adli ogrenci eklendi";
+            return RedirectToAction("OgrList");
+        }
+        public IActionResult OgrList() {
+            return View(ogrenciler);
+        }
+        public IActionResult ogrHata()
+        {
+            return View();
+        }
+    }
+}
---
+++ H4B1/Models/Ogrenci.cs
@@ -0,0 +1,9 @@
+﻿namespace H4B1.Models
+{
+    public class Ogrenci
+    {
+        public string ogrAdi { get; set; }
+        public string ogrSoyad { get; set; }
+        public string ogrNo { get; set; }
+    }
+}
