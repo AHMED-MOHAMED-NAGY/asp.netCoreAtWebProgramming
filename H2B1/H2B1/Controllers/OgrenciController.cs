4d8ff1c61d5ade82a074c6c7bb179e7da71a7ad5
ahmed <ahmedmohamed6x5@gmail.com>
2025-10-26 15:48:56 +0300

    uploading the 2 week codes to github

---
+++ H2B1/H2B1/Controllers/OgrenciController.cs
@@ -0,0 +1,24 @@
+﻿using H2B1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace H2B1.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public String OgrEkle() {
+            return "Ekle Ogrenci ";
+        }
+
+        public IActionResult OgrGoster() {
+
+            Ogrenci o = new Ogrenci();
+            o.ogrName = "i";
+            return View();
+        }
+    }
+}
