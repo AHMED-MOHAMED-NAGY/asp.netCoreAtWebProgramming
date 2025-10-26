0b1f4caa90719732d47b7f73688d3fedb5338431
ahmed <ahmedmohamed6x5@gmail.com>
2025-10-26 15:49:51 +0300

    uploading the 3.week codes to github

---
+++ H3C2/H3C2/Controllers/OgrenciController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace H3C2.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public IActionResult ogrEkle()
+        {
+            TempData["Ad"] = "Mehmet => Temp data";
+            ViewData["no"] = 10;
+            ViewBag.msj = "ViewBag";
+            return View();
+        }
+        public IActionResult Deneme1()
+        {
+            TempData["Ad"] = "Mehmet => Temp data";
+            ViewData["no"] = 10;
+            ViewBag.msj = "ViewBag";
+            return RedirectToAction("Deneme2");
+        }
+        public IActionResult Deneme2() 
+        {
+            if (TempData["Ad"] is not null)
+            {
+                string ad = TempData["Ad"].ToString();
+            }
+            return View();
+        }
+    }
+}
