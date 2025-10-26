1e0975613fb90dae4fd6aa8bd6eda2cf8c5a8e5c
ahmed <ahmedmohamed6x5@gmail.com>
2025-10-26 15:50:52 +0300

    uploading the 5.week codes to github

---
+++ H5B1/H5B1/Controllers/OgrenciController.cs
@@ -0,0 +1,42 @@
+﻿using H5B1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace H5B1.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public static List<Ogrenci> ogrenciler = new List<Ogrenci>(); // data gimemsi icin 
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult OgrEkle() 
+        {
+            return View();
+        }
+        public IActionResult OgrEkleOto() { 
+            return View();
+        }
+        public IActionResult OgrKaydet(Ogrenci ogr) {
+            if (ogr.OgrSoyad.Length > 50) { // i must add all thing ...
+                // hata
+            }
+            if (ModelState.IsValid) { //parametre olarak modelim tum kisitleri sagliyorsa true
+                // hata yok
+                // kaydet
+                ogrenciler.Add(ogr);
+                TempData["msj"] = ogr.OgrAd + " adli ogrenci eklendi";
+                return RedirectToAction("OgrList");
+            }
+            TempData["hata"] = "lutfen tum ogrenci dogru yazi";
+            return RedirectToAction("OgrHata");
+        }
+        public IActionResult OgrList()
+        {
+            return View(ogrenciler);
+        }
+        public IActionResult OgrHata() {
+            return View();
+        }
+    }
+}
---
+++ H5B1/H5B1/Models/Ogrenci.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace H5B1.Models
+{
+    public class Ogrenci
+    {
+        [Display(Name = "Ogrenci Adi")]
+        [Required(ErrorMessage = "Adi alani bos gecilemez")]
+        [MinLength(2,ErrorMessage ="ad min 2 olabilir")]
+        public string OgrAd { get; set; }
+
+
+        [Display (Name = "Ogrenci Soyadi")]
+        [MaxLength(50,ErrorMessage ="max 50 olmasi lazim")]
+        public string OgrSoyad { get; set; }
+
+
+        [Required(ErrorMessage = "Ogrenci No bos gecilemez")]
+        [Display (Name ="Ogrenci No")]
+        public string OgrNo { get; set; }
+
+
+        [Display (Name ="Ogrenci Yas")]
+        [Range(18,25,ErrorMessage ="18 25 arasi olmasi lazim")]
+        public int OgrYas { get; set; }
+    }
+}
